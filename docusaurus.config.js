958cb6a177e8c0f1880b23aca1702a73592b4b71
Tim Lin <e0610k@gmail.com>
2023-01-01 16:53:13 +0800

    feat(新增docs): docs-新增useReducer筆記

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -186,6 +186,17 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        magicComments: [
+          {
+            className: 'theme-code-block-highlighted-line',
+            line: 'highlight-next-line',
+            block: { start: 'highlight-start', end: 'highlight-end' },
+          },
+          {
+            className: 'code-block-error-line',
+            line: 'error-next-line',
+          },
+        ],
       },
       liveCodeBlock: {
         playgroundPosition: 'bottom',
